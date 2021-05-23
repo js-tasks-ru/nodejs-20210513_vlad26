e75e6d6b59af98713da11456928d99b9de6cf271
Vlad Listgarten <listgarten100@gmail.com>
2021-05-23 15:27:28 +0300

    task 5/1

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,16 +1,29 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
-    case 'GET':
+    case 'GET': fs.readFile(filepath, function(error, data) {
+      if (error) {
+        if ((/\//).test(pathname)) {
+          res.statusCode = 400;
+          res.end('Resourse not found!');
+        } else {
+          res.statusCode = 404;
+          res.end('Resourse not found!');
+        }
+      } else {
+        res.end(data);
+      }
+    });
 
       break;
 
